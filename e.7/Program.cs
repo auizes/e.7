78408290c4cb7134e67743d0d99ab0179da69c6d
gustavo.ssilva88 <gustavo.ssilva88@L130570886W10-1>
2022-08-25 14:37:49 -0300

    Adicionar arquivos de projeto.

---
+++ e.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace e._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("digite um numero: ");
+            int n;
+
+            Console.WriteLine("=======================");
+            Console.WriteLine("tabuada");
+            Console.WriteLine("=======================");
+
+            Console.WriteLine(" 01 x " + (1 * n));
+
+
+        }
+    }
+}
